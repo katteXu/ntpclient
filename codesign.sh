6d2b3dc496150cbbfdbcbc93ebbf5e884c415562
katte <417560450@qq.com>
2025-02-14 20:42:10 +0800

    删除文件

--- codesign.sh
+++
@@ -1,8 +0,0 @@
-codesign --sign "Apple Development: loonlady8@gmail.com (SP63Y5698A)" \
- --timestamp \
- --options runtime \
- -f \
- ./target/release/ntpclient
-
-
-codesign -dv --verbose=4 ./target/release/ntpclient  # 查看签名信息
